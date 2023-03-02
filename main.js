22e0e799d2435d9b9e7130f3fe10d6d496c0cbac
nitic04 <68746060+nitic04@users.noreply.github.com>
2023-03-02 13:56:10 -0500

    refined game mechanics
    implemented mechanics for the info tab, removed duplicate colours in colour combination, changed colour of background of column based on what column player is on

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 var color = '#ddd';
+var modal = document.getElementById('info-modal');
+var modal_backdrop = document.getElementById('backdrop');
 
-// solution: green, black, blue, yellow
-var solution = ['rgb(0, 128, 0)', 'rgb(0, 0, 0)', 'rgb(0, 0, 255)', 'rgb(255, 255, 0)'];
+function openInfo(){
+    modal.style.display = "block";
+    modal_backdrop.style.display = "block";
+}
+
+function closeInfo(){
+    modal.style.display = "none";
+    modal_backdrop.style.display = "none";
+}
+
+function generate_solution(){
+    solution = []
+    const colours = ['rgb(0, 128, 0)', 'rgb(255, 0, 0)', 'rgb(0, 0, 255)', 'rgb(0, 0, 0)', 'rgb(255, 255, 255)', 'rgb(255, 255, 0)'];
+    const nums = [0, 1, 2, 3, 4, 5]
+    for (let i = 0; i < 4; i++){
+        result = nums[Math.floor(Math.random()*nums.length)];
+        nums.splice(nums.indexOf(result), 1);
+        solution.push(colours[i])
+    }
+}
+
+window.onload = function() {
+    generate_solution();
+    console.log(solution);
+    document.getElementById('col1').style.backgroundColor = 'rgb(255, 165, 0)';
+  }
+
+// var solution = ['rgb(0, 128, 0)', 'rgb(0, 0, 0)', 'rgb(0, 0, 255)', 'rgb(255, 255, 0)'];
 var current_column = 1;
 var column = '';
 
@@ -44,7 +72,6 @@ function getColour(piece_id){
     const element = document.getElementById(piece_id);
     const style = getComputedStyle(element);
     color = style.backgroundColor;
-    // console.log(style.backgroundColor); //get rid of this
 }
 
 function changeColor(selection_id){
@@ -80,9 +107,6 @@ function checkAnswer(){
     box3 = 'box'+String(current_column)+'c3';
     box4 = 'box'+String(current_column)+'c4';
 
-    console.log(red);
-    console.log(white);
-
     for (let i=1; i <= red; i++){
         boxName = 'box' + current_column + 'c' + i;
         box = document.getElementById(boxName);
@@ -101,8 +125,16 @@ function checkAnswer(){
         document.getElementById('ansc3').style.backgroundColor = solution[2];
         document.getElementById('ansc4').style.backgroundColor = solution[3];
         alert('CONGRATULATIONS YOU HAVE WON THE GAME!');
+    } else if (current_column == 10){
+        document.getElementById('ansc1').style.backgroundColor = solution[0];
+        document.getElementById('ansc2').style.backgroundColor = solution[1];
+        document.getElementById('ansc3').style.backgroundColor = solution[2];
+        document.getElementById('ansc4').style.backgroundColor = solution[3];
+        alert('YOU LOST. BETTER LUCK NEXT TIME!');
     }
 
     // keep track of current column
     current_column += 1;
+    document.getElementById('col'+(current_column-1)).style.backgroundColor = 'rgb(128, 0, 128)';
+    document.getElementById('col'+current_column).style.backgroundColor = 'rgb(255, 165, 0)';
 }
